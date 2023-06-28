80e8c471732ead5a1bb6f4f74653982fbdb69840
bmahfouz <b.mahfouz.86@gmail.com>
2023-06-28 06:55:46 +0300

    upgrade to 0.4.0

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/ios/SyncStageSDK_0.3.1.xcframework.zip", checksum: "8f248b034e78396d7cec35331714842d9bcd64482979c04e9910f935e85f04d6"),
-        .binaryTarget(name: "ffmpegkit", url: "https://syncstage.s3.amazonaws.com/sdk/ios/ffmpegkit_5.1.xcframework.zip", checksum: "a32119d46dbc30a51cbb1f01a32cfe8a9043fbda45a3e90bfa5d4df2fa1865a3"),
-        .binaryTarget(name: "libavcodec", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libavcodec_5.1.xcframework.zip", checksum: "24f938f4333edc7863c365bf6afcc7a0af38502557622eb7e829e683c3343c6b"),
-        .binaryTarget(name: "libavdevice", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libavdevice_5.1.xcframework.zip", checksum: "9493272cc7dc112595fc198247e22b1f7c8bd22809a7c84f30556f338a486323"),
-        .binaryTarget(name: "libavfilter", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libavfilter_5.1.xcframework.zip", checksum: "62364029561900972710910febf94096dae210bce5efb3feb7d29f085cd7f50e"),
-        .binaryTarget(name: "libavformat", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libavformat_5.1.xcframework.zip", checksum: "83ee82a8f5a4c86a83e36e95cd880f0a48f96b51fbc85583ca7c8c710be6467c"),
-        .binaryTarget(name: "libavutil", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libavutil_5.1.xcframework.zip", checksum: "88fa6fb650dfbad6bd6eb86af85fd9b3b15183149162283c71b9ad625f80164f"),
-        .binaryTarget(name: "libswresample", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libswresample_5.1.xcframework.zip", checksum: "27b7b8dc8f17fa161f499d040726566ff29f4e4e97e75fa065ae0744a199c534"),
-        .binaryTarget(name: "libswscale", url: "https://syncstage.s3.amazonaws.com/sdk/ios/libswscale_5.1.xcframework.zip", checksum: "52c469fdd066c06fe3f929beec3e9da93bd166e2268d27494e1011c956f61688")
+        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/ios/SyncStageSDK_0.4.0.xcframework.zip", checksum: "a6cec084b6ad38283cf87f50e9c90a4c93a07ba5889b78fe58b17076f4a71310"),
+        .binaryTarget(name: "ffmpegkit", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/ffmpegkit_5.1.7.xcframework.zip", checksum: "a6b1eeb188363d0785ffede7c3fc299936a4bb0c44cf2c2435737c261b7003ea"),
+        .binaryTarget(name: "libavcodec", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavcodec_5.1.7.xcframework.zip", checksum: "4912132678d280c514fb12b0f19ee6d9b8f2ef21ff668323221cb1710b4167a8"),
+        .binaryTarget(name: "libavdevice", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavdevice_5.1.7.xcframework.zip", checksum: "a960e0dd84a74a51e7f854896a5868031005c79558bd8dccff5ce75958ad367d"),
+        .binaryTarget(name: "libavfilter", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavfilter_5.1.7.xcframework.zip", checksum: "5c3084fd69e67570247d9ae166aef716581a65d3955954ec8e3a95763d95b9a0"),
+        .binaryTarget(name: "libavformat", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavformat_5.1.7.xcframework.zip", checksum: "98d36d1c921727249496facc1941eda4602fd6996834526559da6f1e2d8a42c8"),
+        .binaryTarget(name: "libavutil", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavutil_5.1.7.xcframework.zip", checksum: "29f37ae80c5a58da56eda4f55d1e526230fed6d1c51db9deb61595cb701e8dea"),
+        .binaryTarget(name: "libswresample", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libswresample_5.1.7.xcframework.zip", checksum: "c98d6284f2c08163ff4627a371c704a2dfbe2a524e38855e1b370cdee9b9a547"),
+        .binaryTarget(name: "libswscale", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libswscale_5.1.7.xcframework.zip", checksum: "c5ffaea2f1a11581df300fb12af2b0be231e357016112e9d8dcf2a996cf8841b")
     ],
     swiftLanguageVersions: [.v5]
 )
