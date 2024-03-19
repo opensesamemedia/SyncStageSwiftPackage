dc1ecfd12f0303a3611bb6504b0feaadf467d456
bmahfouz <b.mahfouz.86@gmail.com>
2024-03-19 13:53:40 +0200

    update to 0.5.2

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SyncStageSDK", url: "https://public.sync-stage.com/sdk/ios/0.5.1/SyncStageSDK_0.5.1.xcframework.zip", checksum: "8d3a1b1815e254a08d70d02f128585748664cc1f5146991de22c88f21c0dc94e"),
-        .binaryTarget(name: "ffmpegkit", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/ffmpegkit_5.1.7.xcframework.zip", checksum: "a6b1eeb188363d0785ffede7c3fc299936a4bb0c44cf2c2435737c261b7003ea"),
-        .binaryTarget(name: "libavcodec", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavcodec_5.1.7.xcframework.zip", checksum: "4912132678d280c514fb12b0f19ee6d9b8f2ef21ff668323221cb1710b4167a8"),
-        .binaryTarget(name: "libavdevice", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavdevice_5.1.7.xcframework.zip", checksum: "a960e0dd84a74a51e7f854896a5868031005c79558bd8dccff5ce75958ad367d"),
-        .binaryTarget(name: "libavfilter", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavfilter_5.1.7.xcframework.zip", checksum: "5c3084fd69e67570247d9ae166aef716581a65d3955954ec8e3a95763d95b9a0"),
-        .binaryTarget(name: "libavformat", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavformat_5.1.7.xcframework.zip", checksum: "98d36d1c921727249496facc1941eda4602fd6996834526559da6f1e2d8a42c8"),
-        .binaryTarget(name: "libavutil", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libavutil_5.1.7.xcframework.zip", checksum: "29f37ae80c5a58da56eda4f55d1e526230fed6d1c51db9deb61595cb701e8dea"),
-        .binaryTarget(name: "libswresample", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libswresample_5.1.7.xcframework.zip", checksum: "c98d6284f2c08163ff4627a371c704a2dfbe2a524e38855e1b370cdee9b9a547"),
-        .binaryTarget(name: "libswscale", url: "https://syncstage.s3.amazonaws.com/sdk/ios/5.1.7/libswscale_5.1.7.xcframework.zip", checksum: "c5ffaea2f1a11581df300fb12af2b0be231e357016112e9d8dcf2a996cf8841b")
+        .binaryTarget(name: "SyncStageSDK", url: "https://public.sync-stage.com/sdk/ios/0.5.2/SyncStageSDK_0.5.2.xcframework.zip", checksum: "a88076ab2a5341ba7316580fbf2a69e808b23ecd4ea5a629286a4192cb642187"),
+        .binaryTarget(name: "ffmpegkit", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/ffmpegkit_5.1.8.xcframework.zip", checksum: "8c2923cf9df3fac9a75542164daaa057a5f5a9f539c4efe075991b0a295df352"),
+        .binaryTarget(name: "libavcodec", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libavcodec_5.1.8.xcframework.zip", checksum: "6d2d3fb95dddaff85933ec0abe591168cbbedf5d68ef8fc309eeb697586b751d"),
+        .binaryTarget(name: "libavdevice", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libavdevice_5.1.8.xcframework.zip", checksum: "4f1f12770933b9323d398df3ab983ffdac6d12ade2b76c85db7b04793df2a40d"),
+        .binaryTarget(name: "libavfilter", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libavfilter_5.1.8.xcframework.zip", checksum: "f30925762118b9bbb78836525524491ae71e9976ad56e0c3c2a1952f58c5d75a"),
+        .binaryTarget(name: "libavformat", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libavformat_5.1.8.xcframework.zip", checksum: "56357027a4d3f199effe5e10315779ad00ec7ba2da2899031ac408b019b378ec"),
+        .binaryTarget(name: "libavutil", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libavutil_5.1.8.xcframework.zip", checksum: "993b2eaea2301a07ce11e2dad622b6fb675391e22f665eb55c4f1a9664cd5d76"),
+        .binaryTarget(name: "libswresample", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libswresample_5.1.8.xcframework.zip", checksum: "ad59782f29e04b2a0ecaea4d44b892384d1bf9554ffc4b38c6126b9ff1680e11"),
+        .binaryTarget(name: "libswscale", url: "https://public.sync-stage.com/sdk/ios/ffmpeg/5.1.8/libswscale_5.1.8.xcframework.zip", checksum: "9698e7e41858ab2ffa4a60366e22df7807fc3aa13df51881cd6cb72570931a6f")
     ],
     swiftLanguageVersions: [.v5]
 )
